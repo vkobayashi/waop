751bda5ac248e8cc2d674164533a74ae1cbb229d
vkobayashi <vladimer.kobayashi@gmail.com>
2018-11-14 15:02:51 +0100

    topic modeling r

--- bibtexr.r
+++ bibtexr.r
@@ -21,6 +21,7 @@ bib_journal = bib[grepl("journal = ", bib)]
 
 #d.bibEQL  = as.data.frame(bib)
 
+load("employ_journals_data.RData")
 
 ###################
 ### Topic Modeling
@@ -28,9 +29,22 @@ bib_journal = bib[grepl("journal = ", bib)]
 
 library(topicmodels)
 library(tm)
-docs2<-VCorpus(VectorSource(bib_abstracts))
-docs <-docs2
 
+
+bibfdt_complete$ABSTRACT <- tolower(bibfdt_complete$ABSTRACT)
+
+bibfdt_complete$ABSTRACT = gsub(pattern="[^[:alnum:]]", replacement=" ", bibfdt_complete$ABSTRACT)
+bibfdt_complete$ABSTRACT = gsub(pattern="¹", replacement=" ", bibfdt_complete$ABSTRACT)
+
+bibfdt_complete$ABSTRACT = removeNumbers(bibfdt_complete$ABSTRACT)
+bibfdt_complete$ABSTRACT = stripWhitespace(removeWords(bibfdt_complete$ABSTRACT,c("â","NA")))
+bibfdt_complete$ABSTRACT = trimws(bibfdt_complete$ABSTRACT)
+
+bibfdt_complete= bibfdt_complete[ABSTRACT!=""]
+
+
+docs2<-VCorpus(VectorSource(bibfdt_complete$ABSTRACT))
+docs <-docs2
 # docs<-tm_filter(docs, FUN=function(x) !any(grep("image original",x)))
 # docs<-tm_filter(docs, FUN=function(x) !any(grep("paperback",x)))
 # docs<-tm_filter(docs, FUN=function(x) !any(grep("www",x)))
@@ -38,45 +52,47 @@ docs <-docs2
 # docs<-tm_filter(docs, FUN=function(x) !any(grep("(C) [[:digit:]]{4} Elsevier B.V. All rights reserved.}",content(x))))
 #docs <- tm_map(docs, tolower)
 
-sent=docs[[200]]$content
-mytransformations<-function(sent){
-  sent<-gsub(pattern="[[:digit:]]+","",sent)
-  sent<-gsub(pattern="Abstract = {","",sent, fixed=TRUE)
-  sent<-gsub(pattern="(C)  Elsevier B.V. All rights reserved.},","",sent, fixed=TRUE)
-  sent <- gsub("[[:space:]]?e\\.g\\.[[:space:]]?", " ", sent)
-  sent <- gsub("([[:space:]]+)e\\.?t\\.?c([[:space:]]+)", " ", sent)
-  sent <- gsub("[[:space:]]?i\\.e\\.[[:space:]]?", " ", sent)
-  sent <- gsub("([[:space:]]+)\\?([[:space:]]+)"," ", sent)
-  sent <- gsub("\\,"," ", sent)
-  sent <- gsub("\\;"," ", sent)
-  sent <- gsub("\\:"," ", sent)
-  sent <- gsub("\\("," ", sent)
-  sent <- gsub("\\)"," ", sent)
-  # sent <- gsub("?"," ", sent)
-  # sent <- gsub("?"," ", sent)
-  sent <- gsub('"'," ", sent)
-  sent <- gsub("[\\.+[:space:]]"," ", sent)
-  # <- gsub("?s", " ", sent)
-  sent <- gsub("'s", " ", sent)
-  #sent <- gsub("s?", " ", sent)
-  sent <- gsub("s'", " ", sent)
-  #sent <- gsub("([[:space:]]+)[[:digit:]]+([[:space:]]+)", " ", sent)
-  sent <- gsub("\\-", " ", sent)
-  #sent <-gsub("<[[:digit:]]+>","", sent)
-  sent <-gsub("/","", sent)
-  #  sent<-gsub("w & o", "w&o", sent)
-  #  sent<-gsub("work & organizational", "work&organizational", sent)
-  sent <- str_trim(sent)
-  return(sent)
-}
+# sent=docs[[200]]$content
+# mytransformations<-function(sent){
+#   sent<-gsub(pattern="[[:digit:]]+","",sent)
+#   sent<-gsub(pattern="Abstract = {","",sent, fixed=TRUE)
+#   sent<-gsub(pattern="(C)  Elsevier B.V. All rights reserved.},","",sent, fixed=TRUE)
+#   sent <- gsub("[[:space:]]?e\\.g\\.[[:space:]]?", " ", sent)
+#   sent <- gsub("([[:space:]]+)e\\.?t\\.?c([[:space:]]+)", " ", sent)
+#   sent <- gsub("[[:space:]]?i\\.e\\.[[:space:]]?", " ", sent)
+#   sent <- gsub("([[:space:]]+)\\?([[:space:]]+)"," ", sent)
+#   sent <- gsub("\\,"," ", sent)
+#   sent <- gsub("\\;"," ", sent)
+#   sent <- gsub("\\:"," ", sent)
+#   sent <- gsub("\\("," ", sent)
+#   sent <- gsub("\\)"," ", sent)
+#   # sent <- gsub("?"," ", sent)
+#   # sent <- gsub("?"," ", sent)
+#   sent <- gsub('"'," ", sent)
+#   sent <- gsub("[\\.+[:space:]]"," ", sent)
+#   # <- gsub("?s", " ", sent)
+#   sent <- gsub("'s", " ", sent)
+#   #sent <- gsub("s?", " ", sent)
+#   sent <- gsub("s'", " ", sent)
+#   #sent <- gsub("([[:space:]]+)[[:digit:]]+([[:space:]]+)", " ", sent)
+#   sent <- gsub("\\-", " ", sent)
+#   #sent <-gsub("<[[:digit:]]+>","", sent)
+#   sent <-gsub("/","", sent)
+#   #  sent<-gsub("w & o", "w&o", sent)
+#   #  sent<-gsub("work & organizational", "work&organizational", sent)
+#   sent <- str_trim(sent)
+#   return(sent)
+# }
 
-docs<-tm_map(docs, content_transformer(mytransformations))
+#docs<-tm_map(docs, content_transformer(mytransformations))
 
 docs <- tm_map(docs, removeWords, stopwords("english"))
-docs <- tm_map(docs, removePunctuation)
+#docs <- tm_map(docs, removePunctuation)
 
 docs <- tm_map(docs, stripWhitespace)
 docs<-tm_map(docs, content_transformer(str_trim))
+
+docs[[1]]$content
 
 # sent_token_annotator<-Maxent_Sent_Token_Annotator()
 # word_token_annotator<-Maxent_Word_Token_Annotator()
@@ -97,25 +113,61 @@ docs<-tm_map(docs, content_transformer(str_trim))
 # }
 
 
-#docs<-tm_map(docs, retain_poswords)
-docs<-tm_filter(docs, FUN=function(x) !is.null(x))
+docs = tm_filter(docs, FUN=function(x) content(x)!="")
+length(docs)
 
 
-#docs <- tm_map(docs, stemDocument)
-#docs <- tm_map(docs, PlainTextDocument)
-dtm<-DocumentTermMatrix(docs, control=list(minWordLength=3))
+dtm = DocumentTermMatrix(docs, control=list(removeNumbers=TRUE))
+
+dim(dtm) # 8926 26454
+head(dimnames(dtm)[[2]],20)
+# save(educ_dtm, file="educ_dtm_big.rda")
+# load("educ_dtm_big.rda")
 
 summary(col_sums(dtm))
+names(head(sort(col_sums(dtm), decreasing = TRUE),200))
 
-term_tfidf<-tapply(dtm$v/row_sums(dtm)[dtm$i], dtm$j,mean) * log2(nDocs(dtm)/col_sums(dtm>0))
+term_tf = col_sums(dtm>0)
+
+summary(term_tf)
+dtm = dtm[, term_tf > 1]
+dtm=dtm[row_sums(dtm)>0,]
+summary(col_sums(dtm))
+dim(dtm) # 8926     14440
+
+term_tfidf = tapply(dtm$v/row_sums(dtm)[dtm$i],dtm$j, mean) *
+  log2(nDocs(dtm)/col_sums(dtm>0))
+
 summary(term_tfidf)
-
-dtm<-dtm[, term_tfidf >= 0.11]
-dtm<-dtm[row_sums(dtm)>0,]
+dtm = dtm[, term_tfidf >= .09]
+dtm=dtm[row_sums(dtm)>0,]
 summary(col_sums(dtm))
+dim(dtm) # 8922     9734
+names(head(sort(col_sums(dtm), decreasing = TRUE),200))
+
+dtm$dimnames$Terms[1:50]
+
+
+
+# #docs<-tm_map(docs, retain_poswords)
+# docs<-tm_filter(docs, FUN=function(x) !is.null(x))
+# 
+# 
+# #docs <- tm_map(docs, stemDocument)
+# #docs <- tm_map(docs, PlainTextDocument)
+# dtm<-DocumentTermMatrix(docs, control=list(minWordLength=3))
+# 
+# summary(col_sums(dtm))
+# 
+# term_tfidf<-tapply(dtm$v/row_sums(dtm)[dtm$i], dtm$j,mean) * log2(nDocs(dtm)/col_sums(dtm>0))
+# summary(term_tfidf)
+# 
+# dtm<-dtm[, term_tfidf >= 0.11]
+# dtm<-dtm[row_sums(dtm)>0,]
+# summary(col_sums(dtm))
 
 library("topicmodels")
-k <- 100
+k <- 50
 SEED <- 2010
 joop_TM <-
   list(VEM = LDA(dtm, k = k, control = list(seed = SEED)),
@@ -127,6 +179,11 @@ joop_TM <-
        CTM = CTM(dtm, k = k,
                  control = list(seed = SEED,
                                 var = list(tol = 10^-4), em = list(tol = 10^-3))))
+
+
+employa_topicCTM =CTM(dtm, k = k,
+                      control = list(seed = SEED,
+                                     var = list(tol = 10^-4), em = list(tol = 10^-3)))
 
 sapply(joop_TM[1:2], slot, "alpha")
 
